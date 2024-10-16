2f8c9f949c4035d45db7cb35b11d1822556eeaa6
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-10-16 08:19:39 -0300

    feat: create ship factory function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import './reset.css';
 import './styles.css';
+
+const ship = function createShip(size) {
+  const length = size;
+  let hits = 0;
+  let sunk = false;
+
+  const hit = function takeHit() {
+    hits += 1;
+  };
+
+  const isSunk = function isShipSunk() {
+    if (hits >= length) {
+      sunk = true;
+    }
+
+    return sunk;
+  };
+
+  return { hit, isSunk };
+};
+
+export { ship };
