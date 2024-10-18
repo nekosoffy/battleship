e305b8667475f47a9b0162d84c760358fbe1e0bb
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-10-18 17:56:43 -0300

    feat: create gameboard factory function

---
+++ src/scripts/gameboard.js
@@ -0,0 +1,99 @@
+const gameboard = function createGameboard() {
+  const board = Array.from({ length: 10 }, () => Array(10).fill(null));
+  const invalidCoordinates = [];
+  const unsinkedShips = [];
+  let isHorizontal = true;
+
+  const grid = function getGrid(x, y) {
+    return board[x][y];
+  };
+
+  const rotateShip = function rotateShipPlacement() {
+    isHorizontal = !isHorizontal;
+  };
+
+  const placeShip = function placeShipOnBoard(coordinates, object) {
+    const { length } = object;
+    const row = board[coordinates[0]];
+    const centerCol = coordinates[1];
+
+    if (row[centerCol] !== null) {
+      return false;
+    }
+
+    if (isHorizontal) {
+      const leftIndex = centerCol - Math.floor(length / 2);
+      const rightIndex = leftIndex + length - 1;
+
+      if (leftIndex < 0 || rightIndex >= 10) {
+        return false;
+      }
+
+      const shipPosition = row.slice(leftIndex, rightIndex + 1);
+
+      if (shipPosition.every((el) => el === null)) {
+        for (let i = leftIndex; i <= rightIndex; i++) {
+          row[i] = object;
+        }
+      } else {
+        return false;
+      }
+
+      unsinkedShips.push(object);
+    }
+
+    if (!isHorizontal) {
+      const upperIndex = coordinates[0] - Math.floor(length / 2);
+      const lowerIndex = upperIndex + length - 1;
+
+      for (let i = upperIndex; i <= lowerIndex; i++) {
+        if (board[i] === undefined || board[i][coordinates[1]] !== null) {
+          return false;
+        }
+      }
+
+      for (let i = upperIndex; i <= lowerIndex; i++) {
+        board[i][coordinates[1]] = object;
+      }
+
+      unsinkedShips.push(object);
+    }
+
+    return true;
+  };
+
+  const receiveAttack = function receiveEnemyAttack(x, y) {
+    if (
+      invalidCoordinates.some((element) => element[0] === x && element[1] === y)
+    ) {
+      return false;
+    }
+
+    const target = board[x][y];
+    if (typeof target === 'object' && target !== null) {
+      target.hit();
+      invalidCoordinates.push([x, y]);
+
+      if (target.isSunk() === true) {
+        unsinkedShips.splice(unsinkedShips[unsinkedShips.indexOf(target)], 1);
+      }
+    } else if (target === null) {
+      board[x][y] = 'shot';
+      invalidCoordinates.push([x, y]);
+    }
+
+    return true;
+  };
+
+  const shipsSank = function checkAllShipsSank() {
+    if (!unsinkedShips.length) {
+      return true;
+    }
+
+    return false;
+  };
+
+  return { grid, rotateShip, placeShip, receiveAttack, shipsSank };
+};
+
+export default gameboard;
